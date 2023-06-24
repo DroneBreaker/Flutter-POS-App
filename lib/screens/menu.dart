b5fc96bf3be7bd46ca4d3dc6a7b6ca9b960ea5c4
DroneBreaker <brownierz01@gmail.com>
2023-06-24 21:35:36 +0100

    Making develop bramch

--- lib/screens/menu.dart
+++ lib/screens/menu.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:restaurant_pos/widgets/app-text.dart';
 
 class MenuPage extends StatefulWidget {
   const MenuPage({super.key});
@@ -10,6 +11,21 @@ class MenuPage extends StatefulWidget {
 class _MenuPageState extends State<MenuPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+        body: Column(
+      children: [
+        Center(
+          child: Column(
+            children: [
+              CircleAvatar(),
+              AppText(text: 'Choose Your Preferred Dish', size: 20)
+            ],
+          ),
+        ),
+        SizedBox(
+          height: 20,
+        ),
+      ],
+    ));
   }
 }
