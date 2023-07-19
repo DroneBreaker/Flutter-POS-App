177c7fbdbe4e1b56cf7a7e1de19457ac1b6265de
DroneBreaker <brownierz01@gmail.com>
2023-07-19 11:51:42 +0100

    New

---
+++ lib/screens/preorder/pre_order.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PreOrderScreen extends StatefulWidget {
+  const PreOrderScreen({super.key});
+
+  @override
+  State<PreOrderScreen> createState() => _PreOrderScreenState();
+}
+
+class _PreOrderScreenState extends State<PreOrderScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
