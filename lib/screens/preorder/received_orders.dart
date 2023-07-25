508bee6cd06cb076f1a3965dd2e91a057969e2aa
DroneBreaker <brownierz01@gmail.com>
2023-07-25 11:46:12 +0100

    Initialiased the received page

--- lib/screens/preorder/received_orders.dart
+++ lib/screens/preorder/received_orders.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:restaurant_pos_app/config/colors.dart';
 
 class ReceivedOrderScreen extends StatefulWidget {
   const ReceivedOrderScreen({super.key});
@@ -10,6 +13,12 @@ class ReceivedOrderScreen extends StatefulWidget {
 class _ReceivedOrderScreenState extends State<ReceivedOrderScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+          child: Text(
+        'Received Order Page',
+        style: GoogleFonts.inter(color: AppColor.PrimaryColor, fontSize: 20.sp),
+      )),
+    );
   }
 }
