a14587d7a064c3386bbd4df458e1c6da9e54ae25
DroneBreaker <brownierz01@gmail.com>
2023-08-28 22:49:25 +0100

    Created order type

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provider/provider.dart';
 import 'package:restaurant_pos_app/providers/order_provider.dart';
 import 'package:restaurant_pos_app/utils/route.dart';
+import 'package:flutter_gb_pos_nfc/flutter_gb_pos_nfc.dart';
 
 import 'providers/app_provider.dart';
 
